66f3fae1393974f6118b8fa9fd08dea28466406b
Connor J Thompson <ct770a@gmail.com>
2024-12-15 19:51:20 +1100

    fixed the issues and erm actually we're going back to direct references again

--- Mod.cs
+++ Mod.cs
@@ -1,4 +1,4 @@
-﻿using DSL;
+﻿
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,19 +20,23 @@ namespace TestMod{
         public override void OnGUI(){
 
             long count = -1;
-            if (cached_balancing_obj == null){
-                FieldInfo myField = typeof(EntityBalancingStore).GetField("_entityBalancingScriptableObject", BindingFlags.NonPublic | BindingFlags.Static);
-                if (myField != null)
-                    cached_balancing_obj = (EntityBalancingScriptableObject)myField.GetValue(null);
-                else return;
-            }
-            else count = cached_balancing_obj.parameters.Count;
+            // working reflection version
+            //if (cached_balancing_obj == null){
+            //    FieldInfo myField = typeof(EntityBalancingStore).GetField("_entityBalancingScriptableObject", BindingFlags.NonPublic | BindingFlags.Static);
+            //    if (myField != null)
+            //        cached_balancing_obj = (EntityBalancingScriptableObject)myField.GetValue(null);
+            //    else return;
+            //} else count = cached_balancing_obj.parameters.Count;
+
+            // not working publicized version 
+            var test = EntityBalancingStore._entityBalancingScriptableObject;
+            if (test != null)
+                count = test.parameters.Count;
             
 
-            GUILayout.Label($"test mod, cards: {count}");
+            GUILayout.Label($"test moddy, cards: {count}");
             if (GUILayout.Button("Add Units!!"))
             {
-                GUILayout.Label("click test");
             }
         }
     }
