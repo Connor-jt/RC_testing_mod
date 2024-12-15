25aaf3f018d8e37a954da829746fb387792c4d05
Connor J Thompson <ct770a@gmail.com>
2024-12-15 16:56:05 +1100

    created some sample case code & updated dot net version to correspond to requirements

--- Mod.cs
+++ Mod.cs
@@ -1,12 +1,42 @@
-﻿using System;
+﻿using DSL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
+using UnityEngine;
+using UnityModManagerNet;
 
 namespace TestMod
 {
     public class Main
     {
+        public static string health;
+        public static string ammo;
 
+        static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            modEntry.OnGUI = OnGUI;
+            //modEntry.OnUpdate = OnUpdate;
+            return true;
+        }
+
+
+        static void OnGUI(UnityModManager.ModEntry modEntry)
+        {
+
+            long count = -1;
+            var test = EntityBalancingStore._entityBalancingScriptableObject;
+            if (test != null)
+            {
+                count = test.parameters.Count;
+            }
+
+            GUILayout.Label($"test mod, cards: {count}");
+            if (GUILayout.Button("Add Units!!"))
+            {
+                GUILayout.Label("click test");
+            }
+        }
     }
 }
