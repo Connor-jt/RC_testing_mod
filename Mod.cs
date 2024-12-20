80819f3f3e81c75717871afbca553851dc33d04f
Connor J Thompson <ct770a@gmail.com>
2024-12-20 17:14:04 +1100

    hooked all the parts where a uint could be loaded, and replaced with code to possibly load external units

--- Mod.cs
+++ Mod.cs
@@ -16,7 +16,8 @@ using HarmonyLib;
 
 namespace TestMod{
     public class Moddy : MelonMod{
-        static List<string> entities_to_localize = new List<string>();
+        public static List<string> entities_to_localize = new List<string>();
+        public static Dictionary<string, AssetBundle> mod_bundles = new Dictionary<string, AssetBundle>();
 
         [HarmonyPatch(typeof(EntityBalancingStore), "SetScriptableObject", new Type[] { typeof(EntityBalancingScriptableObject) })]
         private static class UnitPatch {
@@ -48,7 +49,6 @@ namespace TestMod{
                             continue;
                         }
 
-                        entity.parameters.AddRange(bundle_units.parameters);
                         foreach (EntityBalancingParameters curr_entity in bundle_units.parameters){
                             // open localisation files, and process them via the same method as the game
                             TextAsset blueprint_name = (TextAsset)bundle.LoadAsset("Localization - BlueprintName");
@@ -61,6 +61,8 @@ namespace TestMod{
 
                             Melon<Moddy>.Logger.Msg("bundle unit processing success: " + curr_entity.entityId);
                         }
+                        entity.parameters.AddRange(bundle_units.parameters);
+                        mod_bundles.Add(bundle.name, bundle);
                     } catch (Exception ex){
                         Melon<Moddy>.Logger.Msg("bundle unit processing exception: " + ex);
                     }
@@ -71,110 +73,186 @@ namespace TestMod{
         }
 
 
-        [HarmonyPatch(typeof(Loca), "Init")]
-        private static class LocalizationPatch{
-            private static void Postfix(){
-                Melon<Moddy>.Logger.Msg("updating localizations");
+        //[HarmonyPatch(typeof(Loca), "Init")]
+        //private static class LocalizationPatch{
+        //    private static void Postfix(){
+        //        Melon<Moddy>.Logger.Msg("updating localizations");
 
-                foreach (string item in entities_to_localize){
-                    Loca.BlueprintNameDictionary["en-US"][item] = item;
-                    Loca.BlueprintDescriptionDictionary["en-US"][item] = item;
-                    Loca.SkillDescriptionDictionary["en-US"][item] = item;
+        //        foreach (string item in entities_to_localize){
+        //            Loca.BlueprintNameDictionary["en-US"][item] = "placeholder" + item;
+        //            Loca.BlueprintDescriptionDictionary["en-US"][item] = "placeholder" + item;
+        //            //Loca.SkillDescriptionDictionary["en-US"][item] = item;
 
-                    Melon<Moddy>.Logger.Msg("bundle unit localized: " + item);
+        //            Melon<Moddy>.Logger.Msg("bundle unit localized: " + item);
+        //        }
+
+        //    }
+        //}
+
+
+        public static UnityEngine.Object load_from_bundle_or_resource(string path){
+            if (path.Length > 0 && path[0] == '<'){
+                string[] strings = path.Split(new char[] { '>' });
+
+                string asset_bundle = strings[0].Substring(1);
+                string asset_path = strings[1];
+
+                if (mod_bundles.TryGetValue(asset_bundle, out AssetBundle bundle))
+                {
+                    Melon<Moddy>.Logger.Msg("successfully loadaed: " + asset_path + " from bundle: " + asset_bundle);
+                    return bundle.LoadAsset(asset_path);
+                }
+                else
+                {
+                    Melon<Moddy>.Logger.Msg("bundle not found: " + asset_bundle);
+                    return null;
                 }
 
+            } else return Resources.Load(path);
+        }
+
+
+        [HarmonyPatch(typeof(EntityFactory), "InstantiateEntity", new Type[] { typeof(string), typeof(Vector3), typeof(EntityController), typeof(string), typeof(string), typeof(Transform), typeof(UnitRole), typeof(bool) })]
+        private static class EntityFactoryPatch1{
+            public static bool Prefix(ref EntityController __result, string entityId, Vector3 position, EntityController originEntity, string tag = "", string name = "", Transform parentTransform = null, UnitRole additionalRoles = UnitRole.None, bool hasBeenCalledFromAbove = false){
+
+                Melon<Moddy>.Logger.Msg("entity instantiation hook: " + entityId);
+
+                string path = EntityBalancingStore.PrefabLocation(entityId);
+                GameObject gameObject = (((object)parentTransform == null) ? ((GameObject)UnityEngine.Object.Instantiate(load_from_bundle_or_resource(path), position, Quaternion.identity))
+                                                                           : ((GameObject)UnityEngine.Object.Instantiate(load_from_bundle_or_resource(path), position, Quaternion.identity, parentTransform)));
+                if (name != "") gameObject.name = name;
+                if (!string.IsNullOrEmpty(tag)) gameObject.tag = tag;
+                EntityController component = gameObject.GetComponent<EntityController>();
+                component.Init(originEntity);
+                component.SetUniqueEntityId(EntityFactory.NextUniqueEntityId());
+                component.AddRoles(additionalRoles);
+                gameObject.SetActive(value: true);
+                if (Tags.IsPlayer(tag)){
+                    component.ReplaceMaterial(EntityFactory.PlayerMaterial);
+                    component.SetHealthBarColor(GameBalancingStore.PlayerHealthBarColor, GameBalancingStore.PlayerHealthBarBatteredColor);
+                    if (Game.CardUpgrades.TryGetValue(entityId, out var value))
+                        foreach (string item in value)
+                            component.AddUpgrade(item);
+
+                } else if (Tags.IsAi(tag)){
+                    component.ReplaceMaterial(component.IsBuilding ? EntityFactory.AiBuildingMaterial : EntityFactory.AiMaterial);
+                    component.SetHealthBarColor(GameBalancingStore.AiHealthBarColor, GameBalancingStore.AiHealthBarBatteredColor);
+                    if (!component.HasRole(UnitRole.Spawn) && component.IsBuilding)
+                        EntityFactory.AiBuildingCount++;
+                }
+                component.OnHasBeenInstantiated(hasBeenCalledFromAbove);
+                __result = component;
+
+                return true;
             }
+        }
+
+        [HarmonyPatch(typeof(EntityFactory), "CreateBuildingPlacementGhost", new Type[] { typeof(string), typeof(bool) })]
+        private static class EntityFactoryPatch2{
+            public static bool Prefix(ref GameObject __result, string entityId, bool returnInactive = false){
+                Melon<Moddy>.Logger.Msg("ghost building placement hook: " + entityId);
+
+                GameObject gameObject = (GameObject)UnityEngine.Object.Instantiate(load_from_bundle_or_resource(EntityBalancingStore.PrefabLocation(entityId)));
+                EntityController component = gameObject.GetComponent<EntityController>();
+                EntityController.RangeToVisualize visualizeRangeWhileSelected = component.visualizeRangeWhileSelected;
+                UnityEngine.Object.Destroy(component);
+                UnityEngine.Object.Destroy(gameObject.GetComponent<EntityBaseParameters>());
+                UnityEngine.Object.Destroy(gameObject.GetComponent<BoxCollider>());
+                AudioSource[] components = gameObject.GetComponents<AudioSource>();
+                for (int i = 0; i < components.Length; i++)
+                    UnityEngine.Object.Destroy(components[i]);
+
+                UnityEngine.Object.Destroy(gameObject.transform.Find("FogOfWarVisibleArea").gameObject);
+                UnityEngine.Object.Destroy(gameObject.transform.Find("MinimapShape").gameObject);
+                if (visualizeRangeWhileSelected != 0){
+                    GameObject gameObject2 = UnityEngine.Object.Instantiate(EntityFactory.RangeCirclePrefab, gameObject.transform, worldPositionStays: true);
+                    gameObject2.name = "PlacementGhostRangeCircle";
+                    float num;
+                    switch (visualizeRangeWhileSelected){
+                        case EntityController.RangeToVisualize.No:
+                            num = 0f;
+                            break;
+                        case EntityController.RangeToVisualize.SkillRange:
+                            num = (float)EntityBalancingStore.SkillRange(entityId);
+                            break;
+                        case EntityController.RangeToVisualize.WeaponRange:
+                            num = EntityBalancingStore.WeaponRange(entityId, false);
+                            break;
+                        case EntityController.RangeToVisualize.SightRange:
+                            num = (float)EntityBalancingStore.SightRadius(entityId, false);
+                            break;
+                        case EntityController.RangeToVisualize.EffectRadius1:
+                            num = EntityBalancingStore.EffectRadius1(entityId);
+                            break;
+                        case EntityController.RangeToVisualize.EffectRadius2:
+                            num = EntityBalancingStore.EffectRadius2(entityId);
+                            break;
+                        default:
+                            throw new ArgumentOutOfRangeException();
+                    }
+                    gameObject2.transform.localScale = 10f * num * Vector3.one;
+                }
+                if (!returnInactive)
+                {
+                    gameObject.SetActive(value: true);
+                }
+                __result = gameObject;
+                return true;
+            }
+        }
+
+        [HarmonyPatch(typeof(EntityFactory), "CreateEntityMesh", new Type[] { typeof(string), typeof(Transform), typeof(float) })]
+        private static class EntityFactoryPatch3{
+            public static bool Prefix(GameObject __result, string entityId, Transform parentTransform, float scaleMultiplier = 1f){
+                Melon<Moddy>.Logger.Msg("entity mesh creation hook: " + entityId);
+
+		        string text = EntityBalancingStore.PrefabLocation(entityId);
+		        UnityEngine.Object @object = load_from_bundle_or_resource(text);
+		        if (@object == null){
+			        Logger.Warn("Cannot find prefab (" + text + ") for entityId: " + entityId);
+			        __result = null;
+                    return true;
+		        }
+		        GameObject gameObject = (GameObject)UnityEngine.Object.Instantiate(@object, parentTransform);
+		        gameObject.SetLayerRecursively(22, 24);
+		        gameObject.SetActive(value: true);
+		        if (Math.Abs(scaleMultiplier - 1f) > 1E-06f)
+			        gameObject.transform.localScale *= scaleMultiplier;
+
+		        UnityEngine.Object.Destroy(gameObject.GetComponent<EntityController>());
+		        UnityEngine.Object.Destroy(gameObject.GetComponent<EntityBaseParameters>());
+		        UnityEngine.Object.Destroy(gameObject.GetComponent<BoxCollider>());
+		        AudioSource[] components = gameObject.GetComponents<AudioSource>();
+		        for (int i = 0; i < components.Length; i++)
+                    UnityEngine.Object.Destroy(components[i]);
+		        UnityEngine.Object.Destroy(gameObject.GetComponentInChildren<ScaleByChangeableValue>());
+
+
+		        Transform transform = gameObject.transform.Find("UnitSpawnedEffect");
+		        if ((bool)transform) UnityEngine.Object.Destroy(transform.gameObject);
+
+		        Transform transform2 = gameObject.transform.Find("BarCanvases");
+		        if ((bool)transform2) UnityEngine.Object.Destroy(transform2.gameObject);
+
+		        Transform transform3 = gameObject.transform.Find("SelectionCircles");
+		        if ((bool)transform3) UnityEngine.Object.Destroy(transform3.gameObject);
+
+		        Transform transform4 = gameObject.transform.Find("MinimapShape");
+		        if ((bool)transform4) UnityEngine.Object.Destroy(transform4.gameObject);
+
+			    __result = gameObject;
+                return true;
+	        }
         }
 
 
 
         const string export_folder = "C:\\Users\\Joe bingle\\Downloads\\RC modding\\exports\\";
-
         bool has_exported = false;
         public override void OnInitializeMelon()
         {
-
         }
-
-        public void rebuild_entity_store()
-        {
-            EntityBalancingScriptableObject thing = EntityBalancingStore._entityBalancingScriptableObject;
-            EntityBalancingStore._entityBalancingScriptableObject = null;
-
-            EntityBalancingStore.ChangeableIntValueCache.Clear();
-            EntityBalancingStore.ChangeableFloatValueCache.Clear();
-            EntityBalancingStore.SetScriptableObject(thing);
-            Melon<Moddy>.Logger.Msg("rebuilt entities cache!!");
-        }
-
-        public void load_mod_bundles()
-        {
-            // make sure our thing is loaded
-            var entities_store = EntityBalancingStore._entityBalancingScriptableObject;
-            if (entities_store == null){
-                Melon<Moddy>.Logger.Msg("entity store not loaded yet!!");
-                return;
-            }
-
-
-            // locate folder with all asset bundles
-            List<AssetBundle> loaded_bundles = new List<AssetBundle>();
-            string assets_folder = "C:\\Users\\Joe bingle\\Downloads\\RC modding\\mods";
-            foreach (string file in Directory.GetFiles(assets_folder))
-            {
-                try
-                {
-                    AssetBundle mod_bundle = AssetBundle.LoadFromFile(file);
-                    if (mod_bundle != null) loaded_bundles.Add(mod_bundle);
-                }
-                catch (Exception ex)
-                {
-                    Melon<Moddy>.Logger.Msg("bundle opening exception: " + ex);
-                }
-            }
-
-            // foreach asset bundle
-            foreach (AssetBundle bundle in loaded_bundles)
-            {
-                try
-                {
-                    // open the 'units' scriptable object, append structures to entity list
-                    EntityBalancingScriptableObject bundle_units = (EntityBalancingScriptableObject)bundle.LoadAsset("Units");
-                    if (bundle_units == null)
-                    {
-                        Melon<Moddy>.Logger.Msg("bundle unit processing exception: entity balancing obj is null");
-                        continue;
-                    }
-
-                    entities_store.parameters.AddRange(bundle_units.parameters);
-
-                    foreach (EntityBalancingParameters entity in bundle_units.parameters)
-                    {
-                        // open localisation files, and process them via the same method as the game
-                        TextAsset blueprint_name = (TextAsset)bundle.LoadAsset("Localization - BlueprintName");
-                        TextAsset blueprint_desc = (TextAsset)bundle.LoadAsset("Localization - BlueprintDescription");
-                        TextAsset skill_desc = (TextAsset)bundle.LoadAsset("Localization - SkillDescription");
-                        // we may need resource re-routing for this (where we hijack the resource loading to accept asset bundles)
-
-                        // skip processing the localization files, just fill in blank according to the custom entity id's
-                        Loca.BlueprintNameDictionary["en-US"][entity.entityId] = entity.entityId;
-                        Loca.BlueprintDescriptionDictionary["en-US"][entity.entityId] = entity.entityId;
-                        Loca.SkillDescriptionDictionary["en-US"][entity.entityId] = entity.entityId;
-
-
-
-                        Melon<Moddy>.Logger.Msg("bundle unit processing success: " + entity.entityId);
-                    }
-                }
-                catch (Exception ex)
-                {
-                    Melon<Moddy>.Logger.Msg("bundle unit processing exception: " + ex);
-                }
-            }
-        }
-
-
         public override void OnGUI(){
             if (has_exported) return;
 
@@ -187,15 +265,6 @@ namespace TestMod{
             GUILayout.Label($"test moddy, cards: {count}");
             if (test == null) return;
 
-            if (GUILayout.Button("add basic custom unit")){
-                test.parameters.Add(ExampleUnit.get_unit);
-                rebuild_entity_store();
-            }
-
-            if (GUILayout.Button("import custom units from mods")){
-                load_mod_bundles();
-                rebuild_entity_store();
-            }
 
             if (GUILayout.Button("unlock enemy units")){
                 List<EntityBalancingParameters> added_entities = new List<EntityBalancingParameters>();
@@ -218,7 +287,6 @@ namespace TestMod{
                 // then loop back and add all the new units in
                 foreach (var item in added_entities)
                     test.parameters.Add(item);
-                rebuild_entity_store();
             }
 
             if (GUILayout.Button("Export units")){
